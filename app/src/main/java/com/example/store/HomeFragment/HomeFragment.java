ee1d8e7b99ebc40b2f5da9b2fb4c09c5d991fa42
Marjan <marjanbagheri753@gmail.com>
2025-05-19 08:44:18 +0330

    listview in the home page

--- app/src/main/java/com/example/store/HomeFragment/HomeFragment.java
+++ app/src/main/java/com/example/store/HomeFragment/HomeFragment.java
@@ -7,22 +7,36 @@ import androidx.annotation.NonNull;
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.viewpager2.widget.ViewPager2;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.example.store.Api.ApiService;
+import com.example.store.Api.RetrofitClient;
 import com.example.store.MainActivity;
 import com.example.store.R;
 import com.example.store.Viewmodels.AppviewModels;
+import com.example.store.Viewmodels.ProductViewModel;
 import com.example.store.databinding.FragmentHomeBinding;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 
 public class HomeFragment extends Fragment {
 FragmentHomeBinding fragmentHomeBinding;
 MainActivity mainActivity;
-
+   ProductsAdapter productsAdapter;
+    SliderAdapter sliderAdapter;
 
     @Override
     public void onAttach(@NonNull Context context) {
@@ -38,12 +52,30 @@ MainActivity mainActivity;
 
         AppviewModels viewModel = new ViewModelProvider(requireParentFragment()).get(AppviewModels.class);
 
-        SliderAdapter adapter = new SliderAdapter();
-        fragmentHomeBinding.viewpager.setAdapter(adapter);
+        sliderAdapter = new SliderAdapter();
+        fragmentHomeBinding.viewpager.setAdapter(sliderAdapter);
 
         viewModel.getBannerList().observe(getViewLifecycleOwner(), sliderList -> {
-            adapter.setSliderList(sliderList);
+            sliderAdapter.setSliderList(sliderList);
         });
+
+
+
+//نمایش لیست محصولات
+        ProductViewModel pviewModel = new ViewModelProvider(this).get(ProductViewModel.class);
+
+        productsAdapter = new ProductsAdapter();
+        fragmentHomeBinding.recyclerProducts.setLayoutManager(new GridLayoutManager(getContext(), 2)); // 2 آیتم در هر ردیف
+        fragmentHomeBinding.recyclerProducts.setAdapter(productsAdapter);
+
+        pviewModel.getProducts().observe(getViewLifecycleOwner(), productList -> {
+            if (productList != null) {
+                productsAdapter.setProductList(productList);
+            }
+        });
+
+
+
 
 
         return fragmentHomeBinding.getRoot();
@@ -51,4 +83,6 @@ MainActivity mainActivity;
 
 
     }
+
+
 }
---
+++ app/src/main/java/com/example/store/HomeFragment/productModel.java
@@ -0,0 +1,54 @@
+package com.example.store.HomeFragment;
+
+public class productModel {
+    private int id;
+    private String title;
+    private String description;
+    private String price;
+    private String imageUrl;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+
+}
